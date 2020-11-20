20a76baef6946ee30af1a4a7d76d9f81fc20075f
Carlos Encine <contato@carlosencine.com>
2020-11-20 09:26:11 -0300

    Add project files.

---
+++ Box.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Box.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Box.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Box.Data
+{
+    public class Class1
+    {
+    }
+}
